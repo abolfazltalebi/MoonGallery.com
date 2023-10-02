68023dd91bfe268d9a928dc978546dce499d244e
abolfazltalebi <abolfazltalebii18@gmail.com>
2023-10-02 19:22:53 +0330

    changes

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: true,
+});
+var swiper = new Swiper(".slide-content", {
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  spaceBetween: 15,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 2,
+    },
+    576: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-prev",
+    prevEl: ".swiper-button-next",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});

e35307d3a46e8338e1f5b344b1dbb3530bee9ed0
abolfazltalebi <abolfazltalebii18@gmail.com>
2023-10-09 23:24:41 +0330

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,11 @@ var swiper = new Swiper(".slide-content", {
     clickable: true,
   },
 });
+  var swiper = new Swiper(".Swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
